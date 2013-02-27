fad3b6f795e3070b9894852ea5c335675b99111e
Marfusios <m@mkotas.cz>
2013-02-27 11:54:51 +0100

    Second commit

---
+++ src/test/java/Test.java
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: m4r10
+ * Date: 2/27/13
+ * Time: 11:53 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Test {
+}
