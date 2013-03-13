4e1d8bad42403dff1d81d26d8a7b57f8ec413e12
Marfusios <m@mkotas.cz>
2013-03-13 20:15:08 +0100

    Added generated tests.

---
+++ src/test/java/ManagerEntitiesImplTest.java
@@ -0,0 +1,48 @@
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: m4r10
+ * Date: 3/13/13
+ * Time: 8:09 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class ManagerEntitiesImplTest {
+
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void testAddEntity() throws Exception {
+
+    }
+
+    @Test
+    public void testAddEntity() throws Exception {
+
+    }
+
+    @Test
+    public void testRemoveEntity() throws Exception {
+
+    }
+
+    @Test
+    public void testGetEntitiesList() throws Exception {
+
+    }
+
+    @Test
+    public void testGetEntitiesList() throws Exception {
+
+    }
+
+    @Test
+    public void testGetEntitiesList() throws Exception {
+
+    }
+}
